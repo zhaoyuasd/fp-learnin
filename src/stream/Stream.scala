744a9ad55e0bf6f92e5ac0b04f026d32f589c307
zhaoyu <774463266@qq.com>
2019-01-09 09:21:07 +0800

    @mod [A]的加与不加 影响方法的运行

--- src/stream/Stream.scala
+++ src/stream/Stream.scala
@@ -11,22 +11,21 @@ package stream {
       }
 
     // 说实话 到这虽然我写出来了 不过有点看不懂了  获取前n 个值
-      def take[A](n:Int):Stream[A]=  {
-       if(n==0) Empty
+      def take(n:Int):Stream[A]=  //这里如果方法为 take[A] 就会报错  take就运行
         this match {
           case Empty=>Empty
-          case Cons(h,t)=>Cons(h,()=>t().take(n-1))
+          case Cons(h,t)=>if(n>0) Cons(h,()=>t().take(n-1)) else Empty
         }
-     }
+
 
     // 删除前几个
-      def drop[A](n:Int):Stream[A]= this match {
+      def drop(n:Int):Stream[A]= this match {  //这里如果方法为 drop[A] 就会报错  drop就运行
          case Empty =>Empty
          case Cons(_,t)=> if(n>0) t().drop(n-1) else this
       }
 
     // 返回符合条件的值
-    def takeWhile[A](f:A=>Boolean):Stream[A]=this match {
+    def takeWhile(f:A=>Boolean):Stream[A]=this match { //这里如果方法为 takeWhile[A] 就会报错  takeWhile就运行
       case Empty=>Empty
       case Cons(h,t)=> if(f(h())) Cons(h,()=>t().takeWhile(f)) else t().takeWhile(f)
     }
