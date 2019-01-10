8b4bef6acd23067fbdd7a9a0dab8f0b5e2b2a19d
zhaoyu <774463266@qq.com>
2019-01-10 20:32:52 +0800

    @mod unfold方法  看着有点蒙圈方法体

--- src/stream/Stream.scala
+++ src/stream/Stream.scala
@@ -141,7 +141,12 @@ package stream {
       go(0,1)
     }
 
-    def unfold[A,S](z:S)(f:S=>Option[(A,S)]):Stream[A]
+    def unfold[A,S](z:S)(f:S=>Option[(A,S)]):Stream[A]={
+      f(z) match {
+        case Some((a,s))=>cons(a,unfold(s)(f))
+        case None =>Empty:Stream[A]
+      }
+    }
 
   }
 
